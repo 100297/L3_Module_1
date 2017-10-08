542613f4170790ddbf3ca4ec3805c907f7165a02
100297 <league@WorthyMini-6.attlocal.net>
2017-10-08 12:55:52 -0700

    d
    das

--- src/SearchingAndSortingAlgorithms/Algorithms.java
+++ src/SearchingAndSortingAlgorithms/Algorithms.java
@@ -4,8 +4,78 @@ import java.util.List;
 
 public class Algorithms {
 	public static int findBrokenEgg(List<String> eggs) {
-		return 0; //<- this needs changing
+		boolean d = true;
+		int a = 0;
+		if (d = true) {
+			for (int i = 0; i < eggs.size(); i++) {
+
+				if (eggs.get(i) == "cracked") {
+					d = false;
+					a = i;
+					return i;
+				}
+			}
+		} // <- this needs changing
+		System.out.println(a);
+		return 0;
+
 	}
+
+	public static int countPearls(List<Boolean> oysters) {
+		int full = 0;
+		for (int i = 0; i < oysters.size(); i++) {
+			if (oysters.get(i) == true) {
+				full++;
+			}
+		}
+		return full;
+	}
+
+	public static Double findTallest(List<Double> peeps) {
+		Double tallest = 0.0;
+		for (int i = 0; i < peeps.size(); i++) {
+			if (peeps.get(i) > tallest) {
+				tallest = peeps.get(i);
+			}
+		}
+		return tallest;
+
+	}
+
+	public static String findLongestWord(List<String> words) {
+		String a = "";
+		for (int i = 0; i < words.size(); i++) {
+			if (words.get(i).length() > a.length()) {
+				a = words.get(i);
+			}
+		}
+
+		return a;
+
+	}
+public static boolean containsSOS(List<String> message1) {
+	boolean SOS = false;
+	for (int i = 0; i < message1.size(); i++) {
+		System.out.println(message1.get(i));
+		if(message1.get(i).equals( " ... --- ... ")) {
+			
+			SOS = true;
+		}
+	}
+	return SOS;
+}
+
+public static List<String> sortScores(List<Double> results) {
+	for (int i2 = 0; i2 < results.size(); i2++) {
+		
 	
-	//Add other methods here
+	for (int i = 0; i < results.size(); i++) {
+		if(results.get(i) > results.get(i+1)) {
+			String t = results.get(i);
+			
+		}
+		}}
+	return results;
+}
+
 }
